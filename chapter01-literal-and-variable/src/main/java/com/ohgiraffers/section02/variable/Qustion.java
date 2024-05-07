7d691215108a17e5ab0059768e21eaf3c6f336ad
choihyejin <jin22636@gmail.com>
2024-05-07 17:44:18 +0900

    first commit

---
+++ chapter01-literal-and-variable/src/main/java/com/ohgiraffers/section02/variable/Qustion.java
@@ -0,0 +1,31 @@
+package main.java.com.ohgiraffers.section02.variable;
+
+public class Qustion {
+    public static void main(String[] args) {
+
+        /*
+        * 김경리가 사원 10명에게 월급을 입금해줘야 하는 날이다.
+        * 사원의 직책은 대리/과장/차장/부장이 있으며
+        * 대리는 100만원, 과장은 120만원, 차장은 130만원, 부장은 150만원의 월급을 받는다.
+        * 보너스는 각 월급 *0.5 만원을 더해서 주어야 한다.
+        *
+         */
+
+        //화면에 출력되는 형태는 다음과 같아야 한다.
+        //김대리의 월급은 100만원 + 보너스 50만원으로 총 150만원 입금되었습니다.
+
+        int aManager = 100; //대리
+        int bManager = 120; //과장
+        int cManager = 130; //차장
+        int dManager = 150; //부장
+
+        double bouns = (double) aManager / bManager / cManager / dManager;
+
+        double bonus = aManager * 0.5;
+
+        System.out.println("김대리의 월급은;" + aManager + "보너스 50만원으로" + "총" + aManager * bonus  +"만원" + "입금되었습니다");
+
+
+
+    }
+}

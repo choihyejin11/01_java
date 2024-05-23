f05b8b9a9d27c8c718058ba85eb5861a147099f5
choihyejin <jin22636@gmail.com>
2024-05-23 15:03:21 +0900

    chap13

---
+++ chap13-exception/src/main/java/com/ohgiraffers/section02/exception/NegativeException.java
@@ -0,0 +1,13 @@
+package main.java.com.ohgiraffers.section02.exception;
+
+public class NegativeException extends Exception{
+
+    public NegativeException(String message) {
+     supe
+    r(message);
+    }
+}
+
+
+
+
